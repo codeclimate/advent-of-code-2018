09551f7a0eb1e6be83077e29b5aaab3e2ae11187
Chris Hulton <chris@codeclimate.com>
2018-12-11 19:44:24 -0500

    Finish day3 pt 1
    We were so close before. Just swapped the loop order.

--- day3.rb
+++ day3.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/ruby
 
-counters = Hash.new(0)
-
 #1 @ 551,185: 21x10
 #<id> @ <left,top>: <widthxheight>
 
@@ -10,12 +8,11 @@ box = []
 File.read("input/day3.txt").lines do |line|
   _, left, top, width, height = line.match(/^\#\d+ \@ (\d+),(\d+): (\d+)x(\d+)$/).to_a.map(&:to_i)
 
-  # todo: off by 1?
-  (0..(left + width - 1)).each do |x|
-    box[x] ||= []
+  (0..(top + height - 1)).each do |y|
+    box[y] ||= []
 
-    (0..(top + height - 1)).each do |y|
-      box[x][y] ||= 0
+    (0..(left + width - 1)).each do |x|
+      box[y][x] ||= 0
 
       first_x_mark = left
       last_x_mark = first_x_mark + width - 1
@@ -24,17 +21,22 @@ File.read("input/day3.txt").lines do |line|
       last_y_mark = first_y_mark + height - 1
 
       if ((first_x_mark <= x && x <= last_x_mark) && (first_y_mark <= y && y <= last_y_mark))
-        box[x][y] = 1
+        box[y][x] += 1
       end
     end
   end
-
-  box.each_with_index do |rows, x|
-    rows.each_with_index do |cols, y|
-      print "#{box[x][y]} "
-    end
-    print "\n"
-  end
-
-  break
 end
+
+matches = 0
+box.each_with_index do |rows, x|
+  rows.each_with_index do |cols, y|
+    if box[x][y] > 1
+      matches += 1
+    end
+
+    # print "#{box[x][y]} "
+  end
+  # print "\n"
+end
+
+puts "Matches: #{matches}"
